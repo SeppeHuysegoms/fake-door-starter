3906dcd5919a7c51d470b1ee93e7acead708ad32
SeppeHuysegoms <seppe.huysegoms@student.howest.be>
2022-10-18 15:14:55 +0200

    Confettie

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
+import JSConfetti  from  '../node_modules/js-confetti/dist/es/index.js';
 
 const init = () => {
+  const jsConfetti = new JSConfetti();
   const $form = document.querySelector("form");
   $form.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    jsConfetti.addConfetti({ emojis: ["🐶", "🦊", "🐰", "🐻", "🐼", "🐻‍❄️"] });
   });
 }
 
